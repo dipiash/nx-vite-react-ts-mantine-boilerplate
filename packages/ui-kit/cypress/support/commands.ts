6e3fb1bf6fb07d1d8e21f547cc3f8627218a80f0
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2023-01-16 23:39:25 +0200

    Added cypress component testing config

--- packages/ui-kit/cypress/support/commands.ts
+++ packages/ui-kit/cypress/support/commands.ts
@@ -1,5 +1,6 @@
+/// <reference types="cypress" />
 // ***********************************************
-// This example commands.js shows you how to
+// This example commands.ts shows you how to
 // create various custom commands and overwrite
 // existing commands.
 //
@@ -7,18 +8,30 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
-
-// We set up the settings
-addMatchImageSnapshotCommand()
-
-// We also overwrite the command, so it does not take a sceenshot if we run the tests inside the test runner
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-Cypress.Commands.overwrite('matchImageSnapshot', (originalFunction, snapshotName, options) => {
-  if (Cypress.env('ALLOW_SCREENSHOT')) {
-    originalFunction(snapshotName, options)
-  } else {
-    cy.log(`Screenshot comparison is disabled`)
-  }
-})
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
